14222a03999ca79cd64287e505ee29964bfbaf48
ibrahimerginoglu <ibrahim.erginoglu@hisarschool.k12.tr>
2024-09-30 22:43:25 +0300

    blablabla

---
+++ RPS/GameState.swift
@@ -0,0 +1,8 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by ibrahim erginoglu on 30.09.2024.
+//
+
+import Foundation
---
+++ RPS/Sign.swift
@@ -0,0 +1,8 @@
+//
+//  Sign.swift
+//  RPS
+//
+//  Created by ibrahim erginoglu on 30.09.2024.
+//
+
+import Foundation
